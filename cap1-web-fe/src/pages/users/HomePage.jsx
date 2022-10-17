05cc087051e57283cdaa5bb33381981a6f1d3560
nguyen tuan <tuan94pth@gmail.com>
2022-10-17 22:53:09 +0700

    tuan commit

--- cap1-web-fe/src/pages/users/HomePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Sidebar from "../../components/Sidebar";
-import Navbar from "./../../components/Navbar";
-
-export const HomePage = () => {
-  return <Sidebar />;
-};
---
+++ react-antd-admin/src/locales/en-US/documentation/index.ts
@@ -0,0 +1,19 @@
+export const en_US_documentation = {
+  'app.documentation.introduction.title': 'Introduction',
+  'app.documentation.introduction.description': `
+  Our products are developed based on the needs of exam scoring,
+  or check easily. Instead of spending hours on grading.
+  With the help of scanning technology,... Use your phone flexibly and conveniently,
+   Process jobs quickly, and improve accuracy.
+   In addition, the product also helps teachers manage exams,
+   classes and students easily with a friendly and easy to use interface...
+  `,
+  'app.documentation.catalogue.title': 'Catalogue',
+  'app.documentation.catalogue.description': 'Click the catalogue to quickly reach the specified content',
+  'app.documentation.catalogue.list.layout': 'Layout',
+  'app.documentation.catalogue.list.routes': 'Routes',
+  'app.documentation.catalogue.list.request': 'HTTP Request',
+  'app.documentation.catalogue.list.theme': 'Theme',
+  'app.documentation.catalogue.list.typescript': 'Typescript',
+  'app.documentation.catalogue.list.international': 'International',
+};
