e66727e0ac58c019b27de3562a1dac7d35746371
nguyen tuan <tuan94pth@gmail.com>
2022-11-28 22:59:41 +0700

    tuan commit ne

---
+++ src/routes/config.tsx
@@ -0,0 +1,53 @@
+import { FC, ReactElement, useEffect, useState } from 'react';
+import { RouteProps, Navigate, useLocation } from 'react-router';
+import { useIntl } from 'react-intl';
+import { useAuth } from '../context/AuthContext';
+
+export interface WrapperRouteProps extends RouteProps {
+  /** document title locale id */
+  titleId: string;
+}
+
+const WrapperRouteComponent: FC<WrapperRouteProps> = ({ titleId, ...props }) => {
+  const { formatMessage } = useIntl();
+  const { loading, currentUser } = useAuth();
+
+  const { pathname } = useLocation();
+
+  if (titleId) {
+    document.title = formatMessage({
+      id: titleId,
+    });
+  }
+
+  // isLoggedIn = false ===> /login
+  // isLoggedIn = true   ==> /home
+  // isAdmin && isLoggedIn => vào đc /admin/....
+  const isAdmin = currentUser?.role === 'admin';
+
+  console.log('🚀 ~ file: config.tsx ~ line 27 ~ isAdmin', isAdmin);
+
+  // console.log(currentUser);
+
+  if (loading) {
+    return <> loading Server please wait a minute </>;
+  }
+
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+
+  const isRoleAdmin = pathname.split('/')[1] === 'admin';
+
+  if (isAdmin) {
+    return !isRoleAdmin ? <Navigate to="/admin/" /> : (props.element as ReactElement);
+  }
+
+  if (isRoleAdmin) {
+    return <Navigate to="/" />;
+  }
+
+  return props.element as ReactElement;
+};
+
+export default WrapperRouteComponent;
